aa68ce6f694b64e490236fd423699f8ca9228d31
la928800 <lg928800845@163.com>
2019-11-05 21:50:22 +0800

    第二次提交，修改了规格组及参数数据和商品信息

--- ly-item/ly-item-service/src/main/java/com/leyou/item/service/SpecService.java
+++ ly-item/ly-item-service/src/main/java/com/leyou/item/service/SpecService.java
@@ -29,6 +29,11 @@ public class SpecService {
     @Autowired
     private ParamMapper paramMapper;
 
+    /**
+     * 通过id查询规格组数据
+     * @param id
+     * @return
+     */
     public List<SpecGroupDTO> queryGroupByCategory(Long id) {
         //1.因为是通过id查询规格组的信息，所以这里先创建specgroup对象
         SpecGroup specGroup = new SpecGroup();
@@ -44,6 +49,11 @@ public class SpecService {
         return BeanHelper.copyWithCollection(list,SpecGroupDTO.class);
     }
 
+    /**
+     * 通过id查询规格参数的信息
+     * @param gid
+     * @return
+     */
     public List<SpecParamDTO> querySpecParams(Long gid) {
         //1.查询规格组参数和查询规格组的信息逻辑差不多，将参数id封装到specparam对象中
         SpecParam param = new SpecParam();
@@ -57,5 +67,98 @@ public class SpecService {
         //4.将查询的数据转换成specparam对象返回即可
         return BeanHelper.copyWithCollection(params,SpecParamDTO.class);
 
+    }
+
+    /**
+     * 添加规格组信息
+     * @param specGroupDTO
+     */
+    public void addSpecGroup(SpecGroupDTO specGroupDTO) {
+        //1.因为是向数据库中添加数据，所以这里我们把dto的对象转换成specgroup类型的对象
+        SpecGroup specGroup = BeanHelper.copyProperties(specGroupDTO, SpecGroup.class);
+        //2.因为里面封装了cid和name的属性值，所以这里我们直接调取mapper来添加数据
+        int count = specMapper.insertSelective(specGroup);
+        //3.判断count是否不等于1,也就是没有添加成功
+        if (count != 1) {
+            //判处异常信息
+            throw new LyException(ExceptionEnum.INSERT_OPERATION_FAIL);
+        }
+        //规格组的表中并没有中间表来关联，所以这里不用做添加中间表的操作
+    }
+
+    /**
+     * 修改规格组的数据
+     * @param specGroupDTO
+     */
+    public void editSpecGroup(SpecGroupDTO specGroupDTO) {
+        //1.将dto类型的数据转换成specgroup类型的数据
+        SpecGroup specGroup = BeanHelper.copyProperties(specGroupDTO, SpecGroup.class);
+        //2.调用mapper层修改数据
+        int count = specMapper.updateByPrimaryKeySelective(specGroup);
+        if (count != 1) {
+            throw new LyException(ExceptionEnum.UPDATE_OPERATION_FAIL);
+        }
+        //3.没有中间表关联所以不做对中间表的操作
+    }
+
+    /**
+     * 根据id删除规格组的信息
+     * @param id
+     */
+    public void deleteSpecGroupById(Long id) {
+        //1.创建specgroup对象
+        SpecGroup specGroup = new SpecGroup();
+        //2.将id封装到里面，并调取删除的方法
+        specGroup.setId(id);
+        int count = specMapper.deleteByPrimaryKey(specGroup);
+        if (count != 1) {
+            throw new LyException(ExceptionEnum.DELETE_OPERATION_FAIL);
+        }
+        //3.没有中间表关联所以不做对中间表的操作
+    }
+
+    /**
+     * 添加规格参数数据
+     * @param specParamDTO
+     */
+    public void addSpecParam(SpecParamDTO specParamDTO) {
+        //1.转换数据类型
+        SpecParam specParam = BeanHelper.copyProperties(specParamDTO, SpecParam.class);
+        //2.调用Mapper层
+        int count = paramMapper.insertSelective(specParam);
+        //3.判空
+        if (count != 1) {
+            throw new LyException(ExceptionEnum.INSERT_OPERATION_FAIL);
+        }
+    }
+
+    /**
+     * 修改规格参数信息数据
+     * @param specParamDTO
+     */
+    public void editSpecParam(SpecParamDTO specParamDTO) {
+        //1.还是转换数据类型
+        SpecParam specParam = BeanHelper.copyProperties(specParamDTO, SpecParam.class);
+        //2.调用mapper中的方法
+        int count = paramMapper.updateByPrimaryKeySelective(specParam);
+        //3.判空
+        if (count != 1) {
+            throw new LyException(ExceptionEnum.UPDATE_OPERATION_FAIL);
+        }
+    }
+
+    /**
+     * 根据id删除规格参数的信息
+     * @param id
+     */
+    public void deleteSpecParamById(Long id) {
+        //1.创建param对象
+        SpecParam param = new SpecParam();
+        param.setId(id);
+        //2.调用mapper
+        int i = paramMapper.deleteByPrimaryKey(param);
+        if (i !=1) {
+            throw new LyException(ExceptionEnum.DELETE_OPERATION_FAIL);
+        }
     }
 }
--- ly-item/ly-item-service/src/main/java/com/leyou/item/web/SpecController.java
+++ ly-item/ly-item-service/src/main/java/com/leyou/item/web/SpecController.java
@@ -4,11 +4,9 @@ import com.leyou.item.dto.SpecGroupDTO;
 import com.leyou.item.dto.SpecParamDTO;
 import com.leyou.item.service.SpecService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -45,5 +43,77 @@ public class SpecController {
     public ResponseEntity<List<SpecParamDTO>> querySpecParams(@RequestParam("gid") Long gid) {
         //直接返回查询结果即可
         return ResponseEntity.ok(specService.querySpecParams(gid));
+    }
+
+    /**
+     * 添加规格组数据，因为是添加需求所以不需要返回值
+     * @return
+     */
+    @PostMapping("/group")
+    public ResponseEntity<Void> addSpecGroup(@RequestBody SpecGroupDTO specGroupDTO) {
+        //1.调用service方法
+        specService.addSpecGroup(specGroupDTO);
+        //2.返回添加成功的响应
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+
+    }
+
+    /**
+     * 修改规格组的信息
+     * @return
+     */
+    @PutMapping("/group")
+    public ResponseEntity<Void> editSpecGroup(@RequestBody SpecGroupDTO specGroupDTO) {
+        //1.修改和添加的流程差不多，调取service方法,这里没有返回值
+        specService.editSpecGroup(specGroupDTO);
+        //2.响应修改成功给前端
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    /**
+     * 根据id删除规格组的信息
+     * @param id
+     * @return
+     */
+    @DeleteMapping("/group/{id}")
+    public ResponseEntity<Void> deleteSpecGroupById(@PathVariable("id") Long id) {
+        //1.直接调用service来删除，没有返回值,增删改基本都没有返回值
+        specService.deleteSpecGroupById(id);
+        //2.返回删除成功的响应信息
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    /**
+     * 新增规格参数信息
+     * @param specParamDTO
+     * @return
+     */
+    @PostMapping("/param")
+    public ResponseEntity<Void> addSpecParam(@RequestBody SpecParamDTO specParamDTO) {
+        //1.增删改仿照规格组那一套，这里不做过多解释
+        specService.addSpecParam(specParamDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    /**
+     * 修改规格参数信息
+     * @return
+     */
+    @PutMapping("/param")
+    public ResponseEntity<Void> editSpecParam(@RequestBody SpecParamDTO specParamDTO) {
+        specService.editSpecParam(specParamDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    /**
+     * 通过id删除规格参数的数据
+     * @param id
+     * @return
+     */
+    @DeleteMapping("/param/{id}")
+    public ResponseEntity<Void> deleteSpecParamById(@PathVariable("id")Long id) {
+        specService.deleteSpecParamById(id);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+
     }
 }
